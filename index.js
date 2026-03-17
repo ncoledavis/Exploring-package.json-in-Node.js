a8562cfb3e9ab3b7506c51ee1fb198ea4a0fd48c
Nathaniel Davis <nathanielcdavis@yahoo.com>
2026-03-17 16:05:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const express = require('express'); 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
